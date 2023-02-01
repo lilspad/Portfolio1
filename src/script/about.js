e0c129936f76815708500cd853fbe02b6235d7cb
lilspad <lilypaczesniak@outlook.com>
2023-02-01 19:27:20 +0000

    extract hero stuff

--- src/script/about.js
+++ src/script/about.js
@@ -1,25 +1,31 @@
 let about = document.getElementById('about');
 let aboutContainer = document.getElementById('about-container');
-let hero = document.querySelector('.hero-display');
+
 let title = document.querySelector('.about-content .title');
 let p = document.querySelector('.about-content p');
-let all = document.querySelector('*');
 
 
-about.addEventListener('pointerover', () => {
-    p.style.marginTop = '0';
-    title.style.padding = '4px';
+const handleOverAbout = () => {
+    p.style.height = '100%';
+    p.style.margin = 'auto';
+    title.style.padding = '0';
+    title.style.fontSize = '2rem';
     aboutContainer.style.height = '95%';
+    about.style.width = '60%';
     hero.style.marginTop = '1%';
-    hero.style.width = '90%'
-});
+}
 
-about.addEventListener('pointerout', () => {
+const handleOutAbout = () => {
     p.style.marginTop = "100%";
     title.style.padding = '50px';
+    title.style.fontSize = '3.75rem';
     aboutContainer.style.height = '50%';
+    about.style.width = '50%';
     hero.style.marginTop = '4%';
-    hero.style.width = '70%'
-});
+}
+
+about.addEventListener('pointerover', handleOverAbout);
+
+about.addEventListener('pointerout', handleOutAbout);
 
 
