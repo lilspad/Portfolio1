f906f33439c830be534563a5751d5253dc470611
lilspad <lilypaczesniak@outlook.com>
2023-02-01 19:27:37 +0000

    Update skills.js

--- src/script/skills.js
+++ src/script/skills.js
@@ -1,10 +1,48 @@
 let skills = document.getElementById('skills');
-let skillsContent = document.getElementById('skills-content');
+let skillsContainer = document.getElementById('skills-container');
+let shapeTwo1 = document.getElementById('two1')
+let shapeTwo2 = document.getElementById('two2')
+let shapeH1 = document.getElementsByClassName('initials');
+let shapeH2 = document.getElementsByClassName('title')
 
-skills.addEventListener('pointerover', () => {
-    hero.style.width = '90%';
-});
+const handleOverSkills = () => {
 
-skills.addEventListener('pointerout', () => {
-    hero.style.width = '70%';
-});
+    skillsContainer.style.height = '150%';
+    skills.style.width = '100%';
+    shapeTwo1.style.width = '0';
+    shapeTwo2.style.width = '0';
+
+    aboutContainer.style.height = '0';
+    worksContainer.style.height = '0';
+
+    for (let i = 0; i < shapeH2.length; i ++) {
+        shapeH2[i].style.fontSize = "0";
+    }
+
+    for (let i = 0; i < shapeH1.length; i ++) {
+        shapeH1[i].style.fontSize = "0";
+    }
+}
+
+const handleOutSkills = () => {
+
+    for (let i = 0; i < shapeH2.length; i ++) {
+        shapeH2[i].style.fontSize = "3.75rem";
+    }
+
+    for (let i = 0; i < shapeH1.length; i ++) {
+        shapeH1[i].style.fontSize = "7.5rem";
+    }
+
+    skillsContainer.style.height = '50%';
+    skills.style.width = '50%';
+    shapeTwo1.style.width = '25%';
+    shapeTwo2.style.width = '25%';
+
+    aboutContainer.style.height = '50%';
+    worksContainer.style.height = '50%';
+}
+
+skills.addEventListener('pointerover', handleOverSkills);
+
+skills.addEventListener('pointerout', handleOutSkills);
