e8e2b3463257e355e23da8c86a22083f78b69ffd
lilspad <lilypaczesniak@outlook.com>
2023-02-25 18:15:55 +0000

    update (current projects etc)

--- src/script/works.js
+++ src/script/works.js
@@ -3,9 +3,9 @@ let worksContainer = document.getElementById('works-container');
 
 let projects = [];
 
-let names = ['Skincare | eCommerce App', 'Scrabble | React App', 'Capricious | Website', 'KoiBoi | mini game', 'Currently working on', 'More projects'];
-let sources = ['appsrc', 'https://github.com/lilspad/scrabble-app', 'https://github.com/lilspad/capricious-garden', 'https://github.com/lilspad/KoiBoi', '', 'https://github.com/lilspad?tab=repositories'];
-let demos = ['appdemo', 'https://wildwood-scrabble.herokuapp.com/', 'https://dreamy-meringue-2d3f95.netlify.app/', 'https://koi-boi.herokuapp.com/'];
+let names = ['Modest | eCommerce App', 'Some | New App', 'Capricious | Website', 'KoiBoi | mini game', 'Currently working on', 'More projects'];
+let sources = ['https://github.com/lilspad/modest', '', 'https://github.com/lilspad/capricious-garden', 'https://github.com/lilspad/KoiBoi'];
+let demos = ['appdemo', '', 'https://capricious.netlify.app/', 'https://koi-boi.herokuapp.com/'];
 
 function createProject(name, src, demo) {
     return {
@@ -28,14 +28,16 @@ function createElement(project) {
         element.innerHTML = '<h3>' + project.name + '</h3><a href="' + project.src + '"target="blank" class="button source">Source</a><a href="' +
         project.demo + '"target="blank" class="button demo">Demo</a>'; 
     } else if (project.name === "Currently working on") {
-        element.innerHTML = '<div class="more"><h3>' + project.name + '</h3><div class="content"><div><h2>Learning..</h2><p>C#</p><p>React native</p></div>' + 
-        '<div><h2>Developing..</h2><p>Scrabble React App</p><p>Utility Mobile app</p><p>2D RPG game</p></div></div></div>';
+        element.innerHTML = '<div class="more"><h3>' + project.name + '</h3><div class="content"><h2>Developing..</h2>' + 
+        '<div><a href="https://github.com/lilspad?tab=repositories" target="blank">Scrabble React App</a><a href="https://wildwood-scrabble.herokuapp.com/" target="blank" > ( Demo ) </a></div>' + 
+        '<a href="" target="blank">2D RPG game</a></div>' + 
+        '<div class="content"><div><h2>Learning..</h2><p>C#</p><p>MongoDB</p></div></div>';
     } else if (project.name === "More projects") {
         element.innerHTML = '<div class="more"><h3>' + project.name + '</h3><div class="content column">' + 
         '<a href="https://github.com/lilspad/lilspad.github.io" target="blank">This portofolio</a>' + 
-        '<a href="" target="blank">Some website</a>'+ 
-        '<a href="" target="blank">Some website</a>' + 
-        '<a href="' + project.src + '" class="button">GitHub repositories ></a></div></div>';
+        '<a href="https://capricious.netlify.app/" target="blank">Capricious Garden Centre</a>'+ 
+        '<a href="https://github.com/lilspad/silver-sparrows" target="blank">Silver Sparrows</a>' + 
+        '<a href="https://github.com/lilspad?tab=repositories" target="blank" class="button">GitHub repositories ></a></div></div>';
     }
     return element;
 }
